42e56f250cd50ea3ce7836475249cfe52f3e6277
Yuriy Solovyov <yuriy.solovyov@prism.com>
2019-09-30 16:51:39 +0300

    Disable AVFoundation

--- modules/videoio/cmake/detect_avfoundation.cmake
+++ modules/videoio/cmake/detect_avfoundation.cmake
@@ -1,18 +1,19 @@
-if(APPLE)
-  set(HAVE_AVFOUNDATION TRUE)
-  if(IOS)
-    set(libs "-framework AVFoundation" "-framework QuartzCore")
-  else()
-    set(libs
-      "-framework Cocoa"
-      "-framework Accelerate"
-      "-framework AVFoundation"
-      "-framework CoreGraphics"
-      "-framework CoreMedia"
-      "-framework CoreVideo"
-      "-framework QuartzCore")
-  endif()
-  ocv_add_external_target(avfoundation "" "${libs}" "HAVE_AVFOUNDATION")
-endif()
+#if(APPLE)
+#  set(HAVE_AVFOUNDATION TRUE)
+#  if(IOS)
+#    set(libs "-framework AVFoundation" "-framework QuartzCore")
+#  else()
+#    set(libs
+#      "-framework Cocoa"
+#      "-framework Accelerate"
+#      "-framework AVFoundation"
+#      "-framework CoreGraphics"
+#      "-framework CoreMedia"
+#      "-framework CoreVideo"
+#      "-framework QuartzCore")
+#  endif()
+#  ocv_add_external_target(avfoundation "" "${libs}" "HAVE_AVFOUNDATION")
+#endif()
 
+set(HAVE_AVFOUNDATION FALSE)
 set(HAVE_AVFOUNDATION ${HAVE_AVFOUNDATION} PARENT_SCOPE)
